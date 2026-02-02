b6e4be03e557a36dc5c1b400c1d2a08e6bd42a2a
Max-dot668 <maxiyouit@gmail.com>
2026-02-01 17:58:18 -0800

    Implement player scoring functions for tennis scoreboard

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-let playerOneScore = document.getElementById("p1-score")
-let playerTwoScore = document.getElementById("p2-score")
+let p1ScoreEl = document.getElementById("p1-score");
+let p2ScoreEl = document.getElementById("p2-score");
+
+function playerOneScore() {
+    let currentScore = Number(p1ScoreEl.textContent)
+    
+    if (currentScore === 0) {
+        currentScore = 15;
+    } else if (currentScore === 15) {
+        currentScore = 30;
+    } else if (currentScore === 30) {
+        currentScore = 40;
+    } else if (currentScore === 40) {
+        currentScore = "Game";
+    }
+    p1ScoreEl.textContent = currentScore;
+}
+
+function playerTwoScore() {
+    let currentScore = Number(p2ScoreEl.textContent)
+    
+    if (currentScore === 0) {
+        currentScore = 15;
+    } else if (currentScore === 15) {
+        currentScore = 30;
+    } else if (currentScore === 30) {
+        currentScore = 40;
+    } else if (currentScore === 40) {
+        currentScore = "Game";
+    }
+    p2ScoreEl.textContent = currentScore;
+}
+
